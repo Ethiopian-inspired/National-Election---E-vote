8a1b58542e877e3b8275c21bfba7f50f1a6a9c52
AlazarBekele <alazarthe70@gmail.com>
2026-03-13 20:09:02 +0300

    feat: form commit

--- core/public/forms.py
+++ core/public/forms.py
@@ -41,7 +41,7 @@ class SignIn (forms.Form):
         'placeholder' : 'Username'
     }))
 
-    password = forms.CharField (max_length=10, widget=forms.TextInput(attrs={
+    password = forms.CharField (max_length=10, widget=forms.PasswordInput(attrs={
         'class' : 'w-full h-[50px] border-b-2 border-gray-600/15 focus:border-indigo-600 focus:outline-hidden inter_SemiBold placeholder-gray-400/80',
         'placeholder' : 'Password'
     }))
