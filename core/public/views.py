e8c4505d86c5e653bfc4be54169fb695c77d01fc
AlazarBekele <alazarthe70@gmail.com>
2026-04-23 15:43:52 +0300

    fix: Remove unused imports View, form and models

--- core/public/views.py
+++ core/public/views.py
@@ -5,7 +5,6 @@ from .forms import (
     Comptition_request
 )
 from django.contrib import messages
-from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
